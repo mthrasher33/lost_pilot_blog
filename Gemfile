45d4dd9cd14dbfa28c8af0f2bafb4ad9199ce243
Matt Thrasher <Matt@Upwards-MacBook-Pro.local>
2015-08-21 11:44:06 -0500

    Add devise and generate user model

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'devise', '~> 3.5.2'
 gem 'simple_form', '~> 3.1.0'
 gem 'haml', '~> 4.0.7'
 gem 'bootstrap-sass', '~> 3.3.5.1'
+gem 'devise', '~> 3.5.2'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
 	before_action :find_article, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!
 
 	def index
 		@articles = Article.all.order('created_at DESC')
